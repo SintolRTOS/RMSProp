f2919de1881119b57a8df42f8bd0754e11cf25b1
283413152@qq.com <283413152@qq.com>
2019-07-23 21:57:19 +0800

    modify file

--- rmsprop.py
+++ rmsprop.py
@@ -9,6 +9,7 @@ import d2lzh as d2l
 import math
 from mxnet import nd
 
+#梯度优化计算
 def rmsprop_2d(x1, x2, s1, s2):
     g1, g2, eps = 0.2 * x1, 4 * x2, 1e-6
     s1 = gamma * s1 + (1 - gamma) * g1 ** 2
@@ -17,9 +18,11 @@ def rmsprop_2d(x1, x2, s1, s2):
     x2 -= eta / math.sqrt(s2 + eps) * g2
     return x1, x2, s1, s2
 
+#平面计算
 def f_2d(x1, x2):
     return 0.1 * x1 ** 2 + 2 * x2 ** 2
 
+#初始学习率、参数等
 eta, gamma = 0.4, 0.9
+#训练和绘制a
 d2l.show_trace_2d(f_2d, d2l.train_2d(rmsprop_2d))
-
